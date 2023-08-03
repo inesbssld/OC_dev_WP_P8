17ecb892228b015d437824dd89a7a06839297aef
inesbssld <iboussel@yahoo.fr>
2023-08-03 12:02:28 +0200

    amélioration de l'étape 2 avec des ppels à fonctions sur les clicks

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,8 @@ const slides = [
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
-
+/* code étape 3 fonctionnel
+// ajout d'un evenement sur le click des flèches
 let arrow_left = document.getElementById("arrow_left");
 arrow_left.addEventListener("click", () => {
   console.log("Vous avez cliqué sur la flèche gauche");
@@ -27,3 +28,21 @@ let arrow_right = document.getElementById("arrow_right");
 arrow_right.addEventListener("click", () => {
   console.log("Vous avez cliqué sur la flèche droite");
 });
+*/
+
+// Récupération des éléments du carrousel
+const arrowLeft = document.getElementById("arrow_left");
+const arrowRight = document.getElementById("arrow_right");
+
+// Event listeners pour les clics sur les flèches
+arrowLeft.addEventListener("click", actionArrowleftClick);
+arrowRight.addEventListener("click", actionArrowRightClick);
+
+// Fonctions pour gérer les clics sur les flèches
+function actionArrowleftClick() {
+  console.log("Clic sur la flèche gauche");
+}
+
+function actionArrowRightClick() {
+  console.log("Clic sur la flèche droite");
+}
